745b819d4c81e4016c4cc16450135082416ccb18
ilango <ilango@alttab.com.au>
2015-09-16 14:08:59 +0530

    Test Change

--- app/src/main/java/com/eventurers/sample/sampleapplicationdemo/MainActivity.java
+++ app/src/main/java/com/eventurers/sample/sampleapplicationdemo/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("ss","Test to commit git hub");
     }
 
     @Override
